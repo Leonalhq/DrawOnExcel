cc7831a91f694b57ac870d9acc999cb58e66f1c4
chesteryue <chester.yuxi@gmail.com>
2015-08-14 17:43:21 +0800

    add method draw on excel

--- DrawOnExcel.py
+++ DrawOnExcel.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import time
 import sys
+import xlwt
+import xlrd
 
 
 def getPixelsFromImage(image):
@@ -12,16 +14,32 @@ def getPixelsFromImage(image):
         pixels.append(pixelsPerRow)
 
     return pixels
- 
+
+def writePixelsIntoExcel(pixels):
+    workbook = xlwt.Workbook(style_compression=2)  
+    sheet = workbook.add_sheet('PixelImage')
+
+    for row in range(0, len(pixels)):
+        for column in range(0, len(pixels)):
+            (r, g, b) = pixels[row][column]
+            print("draw on cell:(" + str(row) + "," + str(column) + ") RGB:(" + str(r) + "," + str(g) + "," + str(b) + ")")
+            cellName = str(row) + "*" + str(column)
+            xlwt.add_palette_colour(cellName, 0x21)
+            workbook.set_colour_RGB(0x21, r, g, b)
+            style = xlwt.easyxf('pattern: pattern solid, fore_colour ' + cellName)
+            sheet.write(row, column, "", style)
+    workbook.save("PixelImage.xls")
+    
+
 
 def main():
     pixels = getPixelsFromImage(Image.open(sys.argv[1]))
-    print(pixels)
+    writePixelsIntoExcel(pixels)
 
 
 if __name__ == "__main__": 
     startTime = time.time()
     main()
     endTime = time.time()
-    print("\n--------------Finished At:" + time.asctime(time.localtime(endTime))+ "-------------")
+    print("--------------Finished At:" + time.asctime(time.localtime(endTime))+ "-------------")
     print("--------------Time Cost:" + str(endTime - startTime) + "-------------")
