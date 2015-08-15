2f20794f63c174db74f4fe9d5f05ee9538e934d8
ChesterYue <chester.yuxi@gmail.com>
2015-08-15 09:41:33 +0800

    merge cell feature

--- DrawOnExcel.py
+++ DrawOnExcel.py
@@ -6,6 +6,8 @@ import xlwt
 import xlrd
 import math
 
+imageName = sys.argv[1]
+mergeSize = int(sys.argv[2])         #to merge a size of pixel into one color
 
 def getPixelsFromImage(image):
     pixels = []
@@ -20,18 +22,21 @@ def getPixelsFromImage(image):
 def writePixelsIntoExcel(pixels):
     workbook = xlwt.Workbook(style_compression=2)  
     sheet = workbook.add_sheet('PixelImage')
-    workbook.save("./Output" + sys.argv[1] + ".xls")
+    workbook.save("./Output" + imageName + ".xls")
 
-    for row in range(0, len(pixels)):
-        for column in range(0, len(pixels[0])):
-            (r, g, b) = pixels[row][column]
-            print("draw on cell:(" + str(row) + "," + str(column) + ") RGB:(" + str(r) + "," + str(g) + "," + str(b) + ")")
-            (colorName, color) = matchSimilarColor((r, g, b))
-            xlwt.add_palette_colour(str(colorName), colorName) 
-            workbook.set_colour_RGB(colorName, color[0], color[1], color[2])
-            style = xlwt.easyxf('pattern: pattern solid, fore_colour ' + str(colorName))
-            sheet.write(row, column, "", style)
-    workbook.save("Output/" + sys.argv[1] + ".xls")
+    for row in range(0, int(math.ceil(len(pixels) / mergeSize))):
+        for column in range(0, int(math.ceil(len(pixels[0]) / mergeSize))):
+            (r, g, b) = pixels[row * mergeSize][column * mergeSize]
+            print("draw on cell:(" + str(row * mergeSize) + "," + str(column * mergeSize) + ") RGB:(" + str(r) + "," + str(g) + "," + str(b) + ")")
+            (colorNum, color) = matchSimilarColor((r, g, b))
+            xlwt.add_palette_colour(str(colorNum), colorNum) 
+            workbook.set_colour_RGB(colorNum, color[0], color[1], color[2])
+            style = xlwt.easyxf('pattern: pattern solid, fore_colour ' + str(colorNum))
+            
+            for mergedRow in range(row * mergeSize, row * mergeSize + mergeSize):
+                for mergedColumn in range(column * mergeSize, column * mergeSize + mergeSize):
+                    sheet.write(mergedRow, mergedColumn, "", style)
+    workbook.save("Output/" + imageName + "." + str(mergeSize) + ".xls")
 
 
 #xlwt only support 56 colors
@@ -46,7 +51,7 @@ def matchSimilarColor(rgb):
 
 
 def main():
-    pixels = getPixelsFromImage(Image.open("./Input/" + sys.argv[1]))
+    pixels = getPixelsFromImage(Image.open("./Input/" + imageName))
     writePixelsIntoExcel(pixels)
 
 
